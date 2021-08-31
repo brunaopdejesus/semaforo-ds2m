bb9830d0ff17d68d21f64fe31264df0c948eef3c
BRUNA OLIVEIRA PEDROSO DE JESUS <21192918@CFP127ALUNO.LOCAL>
2021-08-31 11:27:19 -0300

    função automatico()

--- semaforo.js
+++ semaforo.js
@@ -1,13 +1,41 @@
 const semaforo = document.getElementById("semaforo")
 
+const semaforoApagado = () => semaforo.src = "img/desligado.png"
+
 const acenderVermelho = () => semaforo.src = "img/vermelho.png"
 
 const acenderAmarelo = () => semaforo.src = "img/amarelo.png"
 
 const acenderVerde = () => semaforo.src = "img/verde.png"
 
+// const automatico = () => acenderVermelho() 
 
 
+
+
+let idVermelho
+function automatico() {
+    acenderVermelho()
+    idVermelho = setTimeout(() => {
+        acenderAmarelo()
+        setTimeout(() => {
+           acenderVerde()
+           setTimeout(() => {
+              automatico() 
+           }, 700); 
+        }, 700);
+    }, 700);
+}
+
+function mudarButtonAutomatico() {
+    const automatico = document.getElementById("automatico")
+    if (automatico.textContent == "Automático") {
+        automatico()
+        automatico.textContent = "Parar"
+    } else {
+        clearInterval(idVermelho)
+    }
+}
 
 
 
@@ -16,4 +44,17 @@ const acenderVerde = () => semaforo.src = "img/verde.png"
 document.getElementById("button-vermelho").addEventListener("click", acenderVermelho)
 document.getElementById("button-amarelo").addEventListener("click", acenderAmarelo)
 document.getElementById("button-verde").addEventListener("click", acenderVerde)
+document.getElementById("button-automatico").addEventListener("click", automatico)
 
+
+// function acenderVermelho() {
+//     semaforo.src = "img/vermelho.png"
+// }
+
+// function acenderAmarelo() {
+//     semaforo.src = "img/amarelo.png"
+// }
+
+// function acenderVerde() {
+//     semaforo.src = "img/verde.png"
+// }
